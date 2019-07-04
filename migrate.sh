350ad4ad5d06990e7cd9998445e929afd30e587f
Tai <tai.thai@alumni.intek.edu.vn>
2019-07-04 23:41:54 +0700

    fix deployment

--- migrate.sh
+++ migrate.sh
@@ -2,4 +2,5 @@
 python manage.py makemigrations polls
 python manage.py sqlmigrate polls 0001
 python manage.py migrate
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python manage.py shell
 exec "$@"
