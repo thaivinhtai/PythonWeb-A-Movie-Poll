3c08279b3b202fc6eeed9bf69c0fbb9bf0c8342a
Tai <thaivinhtai@gmail.com>
2019-07-05 17:27:48 +0700

    completed core engine

--- polls/views.py
+++ polls/views.py
@@ -13,8 +13,14 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+
 
 
 class DetailView(generic.DetailView):
@@ -31,15 +37,6 @@ class DetailView(generic.DetailView):
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
-
-    def get_queryset(self):
-        """
-        Return the last five published questions (not including those set to be
-        published in the future).
-        """
-        return Question.objects.filter(
-            pub_date__lte=timezone.now()
-        ).order_by('-pub_date')[:5]
 
 
 def vote(request, question_id):
