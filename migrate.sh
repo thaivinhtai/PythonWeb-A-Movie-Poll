a956f3b17ca1ee27d4b2d95779867795e9a72c34
Tai <thaivinhtai@gmail.com>
2019-07-12 17:51:16 +0700

    update restful

--- migrate.sh
+++ migrate.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 python manage.py makemigrations polls
 python manage.py sqlmigrate polls 0001
+python manage.py sqlmigrate polls 0002
 python manage.py migrate
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python manage.py shell
 exec "$@"
